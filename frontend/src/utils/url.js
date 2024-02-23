a069d78893b4ebe91e5316c30d9770e6dc3dcc92
Abhay Rajak <mail2rajab01@gmail.com>
2024-02-23 19:32:25 +0530

    cors origin setup

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,2 +1,2 @@
-// export const url = "https://taskify-b9oj.vercel.app";
-export const url = "http://localhost:5000";
+export const url = "https://taskify-b9oj.vercel.app";
+// export const url = "http://localhost:5000";
